a21180ef66e3c41b9ee287df8104ad083520841e
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-26 23:55:28 +0530

    Paperclip added and Configured

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326175441) do
+ActiveRecord::Schema.define(version: 20160326180849) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,9 +19,13 @@ ActiveRecord::Schema.define(version: 20160326175441) do
     t.string   "movie_length"
     t.string   "director"
     t.string   "rating"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "movies", ["user_id"], name: "index_movies_on_user_id"
