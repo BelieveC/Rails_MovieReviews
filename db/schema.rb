99b0b818f84fbc7daee9726af53d3e1658d16b21
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-26 23:27:05 +0530

    Movie controller and Model file updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326174712) do
+ActiveRecord::Schema.define(version: 20160326175441) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160326174712) do
     t.string   "rating"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
+
+  add_index "movies", ["user_id"], name: "index_movies_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
