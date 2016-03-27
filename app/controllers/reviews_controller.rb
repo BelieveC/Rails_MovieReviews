5292f24e2646ecb836acf4a72c7b3e628f7c2f22
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 11:04:27 +0530

    Routes Fixed for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
   # GET /reviews
   # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
-
   # GET /reviews/new
   def new
     @review = Review.new
@@ -26,11 +18,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -43,8 +35,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
+        format.html { redirect_to @movie, notice: 'Review was successfully updated.' }
       else
         format.html { render :edit }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -68,5 +59,9 @@ class ReviewsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:rating, :comment)
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :reviews
   devise_for :users
-  resources :movies
+  resources :movies do 
+    resources :reviews, except: [:show,:index]
+  end
 
   root 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
