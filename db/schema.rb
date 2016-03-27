a41cd9c21b4519a3e53bfbb5579e4210a8f1cc29
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 10:43:11 +0530

    Add Migration movie_id to Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327045527) do
+ActiveRecord::Schema.define(version: 20160327051015) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160327045527) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
