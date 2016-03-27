39e66c7c9e69db496c75f656c6f0ae8f78322b54
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 10:30:16 +0530

    Added REview Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326180849) do
+ActiveRecord::Schema.define(version: 20160327045527) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20160326180849) do
   end
 
   add_index "movies", ["user_id"], name: "index_movies_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
