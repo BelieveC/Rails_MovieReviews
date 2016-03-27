81fd718d2860c48adc8c1f7efac7d4673111506c
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 13:20:55 +0530

    Review rating on show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,11 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
     @reviews =Review.where(movie_id: @movie.id).order(created_at: :desc)
+    if @reviews.blank?
+      @avg = 0
+    else
+      @avg = @reviews.average(:rating).round(2)
+    end
   end
 
   # GET /movies/new
