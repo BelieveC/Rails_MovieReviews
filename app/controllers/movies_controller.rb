0cf580a2bf1e116e0bab8c242d56a81056c80f59
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-06 15:00:19 +0530

    Styling Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only:[:new,:edit,:destroy,:create,:update]
 
   # GET /movies
   # GET /movies.json
